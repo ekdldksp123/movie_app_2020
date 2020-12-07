69ecde0aec6fae90d2cc0d0829f6eef1f01b1c2b
punchlips <ekdldksp123@naver.com>
2020-12-07 20:20:47 +0900

    version 1.1

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -16,7 +16,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
               </li>
             ))}
           </ul>
-          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+          <p className="movie__summary">{summary.slice(0, 120)}...</p>
         </div>
     </div>
   );
